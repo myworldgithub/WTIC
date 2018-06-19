314d9313bd85c6ae1a1b86f710d32472901cf718
z18132030019 <abc123456>
2018-06-19 17:14:51 +0800

    first commit

---
+++ WTIC.go
@@ -0,0 +1,14 @@
+CW18以太坊代币钱包：
+代币中文名称:世界旅游产业链
+代币英文名：World Tourism industry chain
+代币英文缩写：WTIC
+代币LOGO:由我们设计,样板出来后发群里确认
+代币的数量（自己发行的数量）:3.3亿枚
+发行价：1.2元人民币/枚
+接收代币的地址：0xd811413cb83B4dd77688a3dd40bCE88358B1Bea9
+
+
+
+合约地址：0x2a49cd6f8b6239c81cbcc2c43902614ff612bae8
+
+区块浏览器：https://etherscan.io/token/0x2a49cd6f8b6239c81cbcc2c43902614ff612bae8
